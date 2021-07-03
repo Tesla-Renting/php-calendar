35e1a13c7435a0f9e6e614be425263bc0ef23771
Michael Banucu <meet@teslavermietung.info>
2021-07-03 19:32:03 +0200

    first version of import ics logic

---
+++ includes/importics.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * Copyright 2013 Sean Proctor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+if (!defined('IN_PHPC')) {
+    die("Hacking attempt");
+}
+
+// https://gist.github.com/29eafb3b3688fa49546bc14ec434a77c.git
+include '../../importerics/iCal.php';
+
+function importics()
+{
+    global $vars, $phpcdb, $phpcid, $phpc_script;
+
+    if (!is_admin()) {
+        permission_error(__('Need to be admin'));
+        exit;
+    }
+
+    $form_page = "$phpc_script?action=admin#phpc-import-ics";
+    return message_redirect(
+        "good!",
+        $form_page
+    );
+}
