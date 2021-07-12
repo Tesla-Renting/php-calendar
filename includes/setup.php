ebbdc7b135ace229b3c481649ffc1604ef0368ec
Michael Banucu <meet@teslavermietung.info>
2021-07-12 03:01:40 +0200

    test

--- includes/setup.php
+++ includes/setup.php
@@ -86,6 +86,9 @@ if(empty($_SESSION["{$phpc_prefix}uid"])) {
 if(empty($phpc_token))
 	$phpc_token = '';
 
+$_GET = stripslashes_r($_GET);
+$_POST = stripslashes_r($_POST);
+
 $vars = array_merge(real_escape_r($_GET), real_escape_r($_POST));
 
 // Find an appropriate calendar id
